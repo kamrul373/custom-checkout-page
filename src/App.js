df3e80544e1cfc5d13397db2e483b20ec16ef98f
unknown <textile.zero@gmail.com>
2023-01-08 06:09:00 +0600

    mobile responsiveness added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import Checkout from "./components/Checkout";
+import logo from "./assets/img/logo.png"
 function App () {
 	return (
 		<div className="App">
+			<img src={ logo } alt="logo" />
 			<Checkout></Checkout>
 		</div>
 	);
